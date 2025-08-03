64a8e8530acbffd009e03a2e13709ff021aa1608
Büşra Akay <61781887+busraakay@users.noreply.github.com>
2025-08-03 15:29:39 +0300

    Project added.

---
+++ Order.Application/Repository/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Order.Domain.SeedWork;
+
+namespace Order.Application.Repository
+{
+    public interface IOrderRepository:IRepository
+    {
+    }
+}
